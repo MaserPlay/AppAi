7df981a84511efabd904ee680ab1a9932a522554
Dmitriy <super.dima771@yandex.ru>
2023-10-23 21:14:48 +0300

    Fixed Login

--- LoginLib/src/main/java/com/maserplay/loginlib/GlobalVariables.kt
+++ LoginLib/src/main/java/com/maserplay/loginlib/GlobalVariables.kt
@@ -8,40 +8,60 @@ import com.maserplay.loginlib.dialogfragment.AccountLogoutDialog
 import com.maserplay.loginlib.dialogfragment.TooManyAccountsDialog
 import java.util.concurrent.TimeUnit
 
-object GlobalVariables{
+object GlobalVariables {
     const val WEB_ADR_FULL = "https://games.m2023.ru/"
+
     //const val WEB_ADR_FULL = "http://localhost:5000/"
     const val DIALOGFRAGMENT_TAG = "error"
     const val LOGTAG_LOGIN = "LoginTAG"
-    fun GetAC(supportFragmentManager: FragmentManager, con: Context) : Account? {
+    fun GetAC(supportFragmentManager: FragmentManager, con: Context): Account? {
         val acs = AccountManager.get(con).accounts
-        return if (acs.size > 1) { TooManyAccountsDialog().show(supportFragmentManager, DIALOGFRAGMENT_TAG)
+        return if (acs.size > 1) {
+            TooManyAccountsDialog().show(supportFragmentManager, DIALOGFRAGMENT_TAG)
             null
         } else if (acs.isEmpty()) {
             null
         } else {
-            acs[0]
+            var ac = acs[0]
+            if (isAcValid(supportFragmentManager, ac, con)) {
+                ac
+            } else {
+                null
+            }
         }
     }
-    fun GetAC(con: Context) : Account? {
+
+    fun GetAC(con: Context): Account? {
         val acs = AccountManager.get(con).accounts
         return if (acs.size > 1 || acs.isEmpty()) {
             null
         } else {
-            acs[0]
+            var ac = acs[0]
+            if (isAcValid(ac, con)) {
+                ac
+            } else {
+                null
+            }
         }
     }
-    fun isAcValid(supportFragmentManager: FragmentManager, account: Account, con: Context): Boolean{
-        if (isAcValid(account, con)){
+
+    fun isAcValid(
+        supportFragmentManager: FragmentManager,
+        account: Account,
+        con: Context
+    ): Boolean {
+        if (!isAcValid(account, con)) {
             AccountLogoutDialog().show(supportFragmentManager, DIALOGFRAGMENT_TAG)
             return false
         }
         return true
     }
-    fun isAcValid(account: Account, con: Context): Boolean{
+
+    fun isAcValid(account: Account, con: Context): Boolean {
         val curtimeSeconds: Long = TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis())
         val acexptime = AccountManager.get(con).getUserData(account, "exp").toLong()
-        if (curtimeSeconds > acexptime){
+        if (curtimeSeconds > acexptime) {
+            logout(account, con)
             return false
         }
         return true
